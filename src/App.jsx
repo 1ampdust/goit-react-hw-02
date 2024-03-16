04a38b1408c11e4063a480ca8b64315934a20c34
Lampdust <lampdust06@gmail.com>
2024-03-16 20:47:49 +0200

    upd

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [clicks, setClicks] = useState(0);
+
+  // Оголошуємо ефект
+  useEffect(() => {
+    document.title = `You clicked ${clicks} times`;
+  });
+
+  return (
+    <button onClick={() => setClicks(clicks + 1)}>
+      You clicked {clicks} times
+    </button>
+  );
+};
+
+ export default App;
